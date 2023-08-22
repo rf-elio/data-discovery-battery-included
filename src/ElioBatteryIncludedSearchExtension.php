9d7ea962320f3da2b5ce03e51080862410725b6e
Danil Lukov <dl@elio-systems.com>
2023-08-22 19:09:14 +0700

    Initial commit

---
+++ src/ElioBatteryIncludedSearchExtension.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Elio\ElioBatteryIncludedSearchExtension;
+
+use Shopware\Core\Framework\Plugin;
+
+class ElioBatteryIncludedSearchExtension extends Plugin
+{
+}
