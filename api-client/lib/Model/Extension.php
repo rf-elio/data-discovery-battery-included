45ddaa378df9b712fe374d7a7c51b16afeabd67a
Danil Lukov <dl@elio-systems.com>
2024-02-05 18:26:43 +0600

    Added promotions. Fixed  access before initialization property error. Added profile language draft

---
+++ api-client/lib/Model/Extension.php
@@ -0,0 +1,278 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright (c) 2024, elio GmbH.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Elio\ElioBatteryIncludedApiClient\Model;
+
+
+use ArrayAccess;
+use Elio\ElioSearch\Swagger\ClientObjectSerializer;
+use Elio\ElioSearch\Swagger\ModelInterface;
+
+/**
+ * Class Extension
+ * @package lib\Model
+ * @category Shopware
+ * @author elio GmbH <support@elio-systems.com>
+ * @author Danil Lukov <dl@elio-systems.com>
+ * @copyright Copyright (c) 2024, elio GmbH (
+ * https://www.elio-systems.com)
+ */
+class Extension implements ModelInterface, ArrayAccess
+{
+    const DISCRIMINATOR = null;
+
+    protected static $swaggerModelName = 'Extension';
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @var string[]
+     */
+    protected static $swaggerTypes = [
+        'type' => 'string',
+        'data' => 'object',
+    ];
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @var string[]
+     */
+    protected static $swaggerFormats = [
+        'type' => null,
+        'data' => null,
+    ];
+
+    /**
+     * Array of property to type mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function swaggerTypes()
+    {
+        return self::$swaggerTypes;
+    }
+
+    /**
+     * Array of property to format mappings. Used for (de)serialization
+     *
+     * @return array
+     */
+    public static function swaggerFormats()
+    {
+        return self::$swaggerFormats;
+    }
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @var string[]
+     */
+    protected static $attributeMap = [
+        'type' => 'type',
+        'data' => 'data',
+    ];
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @var string[]
+     */
+    protected static $setters = [
+        'type' => 'setType',
+        'data' => 'setData',
+    ];
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @var string[]
+     */
+    protected static $getters = [
+        'type' => 'getType',
+        'data' => 'getData',
+    ];
+
+    /**
+     * Array of attributes where the key is the local name,
+     * and the value is the original name
+     *
+     * @return array
+     */
+    public static function attributeMap(): array
+    {
+        return self::$attributeMap;
+    }
+
+    /**
+     * Array of attributes to setter functions (for deserialization of responses)
+     *
+     * @return array
+     */
+    public static function setters(): array
+    {
+        return self::$setters;
+    }
+
+    /**
+     * Array of attributes to getter functions (for serialization of requests)
+     *
+     * @return array
+     */
+    public static function getters(): array
+    {
+        return self::$getters;
+    }
+
+    /**
+     * The original name of the model.
+     *
+     * @return string
+     */
+    public function getModelName(): string
+    {
+        return self::$swaggerModelName;
+    }
+
+    public function __construct(array $data = null)
+    {
+        $this->container['type'] = isset($data['type']) ? $data['type'] : null;
+        $this->container['data'] = isset($data['data']) ? $data['data'] : null;
+    }
+
+    public function setType($type)
+    {
+        $this->container['type'] = $type;
+
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->container['type'];
+    }
+
+    public function setData($data)
+    {
+        $this->container['data'] = $data;
+
+        return $this;
+    }
+
+    public function getData()
+    {
+        return $this->container['data'];
+    }
+
+    public function listInvalidProperties()
+    {
+        $invalidProperties = [];
+        // TODO: Validation
+        return $invalidProperties;
+    }
+
+    public function valid()
+    {
+        return count($this->listInvalidProperties()) === 0;
+    }
+
+    /**
+     * Returns true if offset exists. False otherwise.
+     *
+     * @param integer $offset Offset
+     *
+     * @return boolean
+     */
+    public function offsetExists($offset)
+    {
+        return isset($this->container[$offset]);
+    }
+
+    /**
+     * Gets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return mixed
+     */
+    public function offsetGet($offset)
+    {
+        return isset($this->container[$offset]) ? $this->container[$offset] : null;
+    }
+
+    /**
+     * Sets value based on offset.
+     *
+     * @param integer $offset Offset
+     * @param mixed $value Value to be set
+     *
+     * @return void
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->container[] = $value;
+        } else {
+            $this->container[$offset] = $value;
+        }
+    }
+
+    /**
+     * Unsets offset.
+     *
+     * @param integer $offset Offset
+     *
+     * @return void
+     */
+    public function offsetUnset($offset)
+    {
+        unset($this->container[$offset]);
+    }
+
+    /**
+     * Gets the string presentation of the object
+     *
+     * @return string
+     */
+    public function __toString()
+    {
+        if (defined('JSON_PRETTY_PRINT')) { // use JSON pretty print
+            return json_encode(
+                ClientObjectSerializer::sanitizeForSerialization($this),
+                JSON_PRETTY_PRINT
+            );
+        }
+
+        return json_encode(ClientObjectSerializer::sanitizeForSerialization($this));
+    }
+}
---
+++ src/Api/Search/ResponseTransformer/PromotionTransformer.php
@@ -0,0 +1,114 @@
+<?php declare(strict_types=1);
+/**
+ * Copyright (c) 2024, elio GmbH.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors
+ * may be used to endorse or promote products derived from this software without
+ * specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace Elio\ElioBatteryIncludedSearchExtension\Api\Search\ResponseTransformer;
+
+
+use Elio\ElioBatteryIncludedApiClient\Model\Extension;
+use Elio\ElioBatteryIncludedApiClient\Model\Result;
+use Elio\ElioSearch\Api\Request\ApiRequest;
+use Elio\ElioSearch\Api\Response\ResponseCollection;
+use Elio\ElioSearch\Api\Search\Response\AdvisorCampaignResponseCollection;
+use Elio\ElioSearch\Api\Search\Response\CampaignFeedbackResponse;
+use Elio\ElioSearch\Api\Search\Response\CampaignFeedbackResponseCollection;
+use Elio\ElioSearch\Api\Transform\ResponseTransformerInterface;
+use Elio\ElioSearch\Core\Exception\InvalidTypeException;
+use Elio\ElioSearch\Swagger\ModelInterface;
+use Shopware\Core\System\SalesChannel\SalesChannelContext;
+
+/**
+ * Class PromotionTransformer
+ * @package Elio\ElioBatteryIncludedSearchExtension\Api\Search\ResponseTransformer
+ * @category Shopware
+ * @author elio GmbH <support@elio-systems.com>
+ * @author Danil Lukov <dl@elio-systems.com>
+ * @copyright Copyright (c) 2024, elio GmbH (
+ * https://www.elio-systems.com)
+ */
+class PromotionTransformer implements ResponseTransformerInterface
+{
+    public const TYPE_PROMOTION = 'promotions';
+
+    public function supports(ModelInterface $model, ApiRequest $request, SalesChannelContext $context): bool
+    {
+        return $model instanceof Result;
+    }
+
+    public function transform(
+        ModelInterface $model,
+        ResponseCollection $responseCollection,
+        SalesChannelContext $context,
+        ApiRequest $request
+    ): void
+    {
+        if (!$model instanceof Result) {
+            throw new InvalidTypeException($model, Result::class);
+        }
+
+        $promotions = [];
+        $extensions = $model->getExtensions() ?? [];
+        /** @var Extension $extension */
+        foreach ($extensions as $extension) {
+            if ($extension->getType() === self::TYPE_PROMOTION) {
+                $promotions[] = $extension;
+            }
+        }
+
+        if (empty($promotions)) {
+            return;
+        }
+
+        $campaignFeedbackResponseCollection = new CampaignFeedbackResponseCollection();
+        $responseCollection->set(CampaignFeedbackResponseCollection::KEY, $campaignFeedbackResponseCollection);
+
+        $advisorCampaignResponseCollection = new AdvisorCampaignResponseCollection();
+        $responseCollection->set(AdvisorCampaignResponseCollection::KEY, $advisorCampaignResponseCollection);
+
+        foreach ($promotions as $promotion) {
+            if (empty($data = $promotion->getData())) {
+                continue;
+            }
+
+            $campaignFeedbackResponseCollection->addCampaignFeedbackResponse(new CampaignFeedbackResponse(
+                'above search result',
+                $this->generatePromotionHtml($data['url'], $data['image']->desktop, $data['name']),
+                true
+            ));
+        }
+    }
+
+    private function generatePromotionHtml(string$url, string $imageUrl, string $alt = ''): string
+    {
+        // TODO: Add config with class?
+        return sprintf('<a href="%s" target="_blank"><img src="%s" alt="%s"></a>', $url, $imageUrl, $alt);
+    }
+}
