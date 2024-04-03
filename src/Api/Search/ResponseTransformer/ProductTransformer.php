0fe0d428b1e2b9061a1878d00cf782d1792a7df7
Andrei Baev <anb@elio-systems.com>
2024-04-03 17:55:17 +0200

    AbstractProductTransformer for search-product-transformer

--- src/Api/Search/ResponseTransformer/ProductTransformer.php
+++ src/Api/Search/ResponseTransformer/ProductTransformer.php
@@ -32,26 +32,17 @@
 
 namespace Elio\ElioBatteryIncludedSearchExtension\Api\Search\ResponseTransformer;
 
-
-use Doctrine\DBAL\Connection;
 use Doctrine\DBAL\Exception;
 use Elio\ElioDataDiscovery\Api\Request\ApiRequest;
 use Elio\ElioDataDiscovery\Api\Response\ResponseCollection;
-use Elio\ElioDataDiscovery\Api\Search\Request\ProductSearchRequest;
-use Elio\ElioDataDiscovery\Api\Search\Response\ProductListingResponse;
-use Elio\ElioDataDiscovery\Api\Transform\ResponseTransformerInterface;
+use Elio\ElioDataDiscovery\Api\Search\ResponseTransformer\AbstractProductTransformer;
 use Elio\ElioDataDiscovery\Core\Exception\InvalidTypeException;
-use Elio\ElioDataDiscovery\Core\Sync\DataTypes\ProductDataType;
-use Shopware\Core\Content\Product\ProductCollection;
-use Shopware\Core\Content\Product\ProductEntity;
-use Shopware\Core\Content\Product\SalesChannel\Listing\ProductListingLoader;
 use Shopware\Core\Framework\DataAbstractionLayer\Exception\InconsistentCriteriaIdsException;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsAnyFilter;
 use Shopware\Core\System\SalesChannel\SalesChannelContext;
 use Elio\ElioDataDiscovery\Swagger\ModelInterface;
 use Elio\ElioBatteryIncludedApiClient\Model\Result;
 use Elio\ElioBatteryIncludedApiClient\Model\SearchRecord;
+use Elio\ElioDataDiscovery\Core\Sync\DataTypes\ProductDataType;
 
 /**
  * Class ProductTransformer
@@ -62,41 +53,22 @@ use Elio\ElioBatteryIncludedApiClient\Model\SearchRecord;
  * @author    Ralf Frommherz <rf@elio-systems.com>
  * @copyright Copyright (c) 2021, elio GmbH (https://www.elio-systems.com)
  */
-class ProductTransformer implements ResponseTransformerInterface
+class ProductTransformer extends AbstractProductTransformer
 {
-    /**
-     * ProductHandler constructor.
-     *
-     * @param ProductListingLoader $listingLoader
-     */
-    public function __construct(
-        private readonly ProductListingLoader $listingLoader,
-        private readonly Connection $connection
-    ) {}
-
-    /**
-     * @inheritDoc
-     */
-    public function supports(ModelInterface $model, ApiRequest $request, SalesChannelContext $context): bool
-    {
-        return $model instanceof Result && $request instanceof ProductSearchRequest;
-    }
-
     /**
      * @param ModelInterface $model
      * @param ResponseCollection $responseCollection
      * @param SalesChannelContext $context
      * @param ApiRequest $request
-     *
-     * @throws InconsistentCriteriaIdsException
+     * @throws InconsistentCriteriaIdsException|Exception
      */
     public function transform(
-        ModelInterface $model,
-        ResponseCollection $responseCollection,
+        ModelInterface      $model,
+        ResponseCollection  $responseCollection,
         SalesChannelContext $context,
-        ApiRequest $request
-    ): void {
-
+        ApiRequest          $request
+    ): void
+    {
         if (!$model instanceof Result) {
             throw new InvalidTypeException($model, Result::class);
         }
@@ -115,64 +87,7 @@ class ProductTransformer implements ResponseTransformerInterface
         // TODO: Resolve main variant
 
         $productNumbers = array_keys($productsData);
-        $productNumberSort = array_flip($productNumbers);
-
-        $criteria = new Criteria();
-        $criteria->addFilter(new EqualsAnyFilter('productNumber', $productNumbers));
-        $criteria->addAssociation('manufacturer');
-
-        /** @var ProductCollection $products */
-        $products = $this->listingLoader->load($criteria, $context)->getEntities();
-
-        // sorts the product collection based on the original ff result order
-        $products->sort(static function (ProductEntity $a, ProductEntity $b) use ($productNumberSort) {
-            $aPosition = $productNumberSort[$a->getProductNumber()] ?? 0;
-            $bPosition = $productNumberSort[$b->getProductNumber()] ?? 0;
-            return $aPosition <=> $bPosition;
-        });
-
-        $listing = $responseCollection->get(ProductListingResponse::class) ?? new ProductListingResponse();
-        $responseCollection->set(ProductListingResponse::class, $listing);
-        $listing->setProducts($products);
-
-        // total count must be corrected by the difference we have for the found products
-        $shouldCount = count($mainNumbers);
-        $isCount = $products->count();
-
-        $difference = $shouldCount - $isCount;
-        $listing->setTotalHits($listing->getTotalHits() - $difference);
+        $listing = $this->parentTransform($productNumbers, $mainNumbers, $responseCollection, $context);
         $listing->setHitsPerPage($model->getRequestParams()['per_page']);
-    }
-
-    /**
-     * Extracts the main and variant product numbers and ids in the right order from the numbers of the search result
-     *
-     * @param array<int, string> $mainNumbers
-     *
-     * @return array<mixed,array<string,mixed>>
-     * @throws Exception
-     */
-    protected function extractMainAndVariantProducts(array $mainNumbers): array
-    {
-        return $this->connection->fetchAllAssociativeIndexed(
-            '# ff product-transformer::extract-product-numbers
-            SELECT
-                IFNULL(child.product_number, parent.product_number) as number,
-                LOWER(HEX(IFNULL(child.id, parent.id))) as id,
-                parent.product_number as parentNumber,
-                LOWER(HEX(parent.id)) as parentId,
-                LOWER(HEX(child.id)) as childId
-            FROM product as parent
-                LEFT JOIN product as child
-                    ON parent.id = child.parent_id
-                    AND parent.version_id = child.version_id
-            WHERE parent.product_number in (:numbers)
-            ORDER BY FIELD(parent.product_number, :numbers), child.product_number
-            ',
-            [
-                'numbers' => $mainNumbers
-            ],
-            ['numbers' => Connection::PARAM_STR_ARRAY]
-        );
     }
 }
