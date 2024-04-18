2c77018c529c7a67126576efe2f9f4f20649cb19
Andrei Baev <anb@elio-systems.com>
2024-04-18 15:55:59 +0200

    fixing the config subscriber

--- src/Configuration/Subscriber/ConfigurationSubscriber.php
+++ src/Configuration/Subscriber/ConfigurationSubscriber.php
@@ -67,7 +67,7 @@ class ConfigurationSubscriber implements EventSubscriberInterface
      */
     public function onConfigurationLoaded(ConfigurationLoadedEvent $event): void
     {
-        $pluginConfig = $this->configService->get(ElioBatteryIncludedSearchExtension::PLUGIN_CONFIG_PREFIX) ?? [];
+        $pluginConfig = $this->configService->get(ElioBatteryIncludedSearchExtension::PLUGIN_CONFIG_PREFIX, $event->getSalesChannelId()) ?? [];
         $configuration = $event->getConfiguration();
         $configuration->addExtension(BatteryIncludedConfiguration::NAME, new BatteryIncludedConfiguration(
             $pluginConfig['collection'] ?? '',
